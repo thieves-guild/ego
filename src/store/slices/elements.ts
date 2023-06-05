4a6b4d273389c6bb601560f2448ad18503bb639c
dmadridy <davidmadridpathway@gmail.com>
2023-06-05 17:19:20 -0500

    Added redux toolit and reac-redux dependencies. Started to configure the global state store using Redux

---
+++ src/store/slices/elements.ts
@@ -0,0 +1,17 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+export const elementSlice = createSlice({
+  name: 'element',
+  initialState: [],
+  reducers: {
+    addElement: (state, action) => {
+      
+    },
+    removeElement: (state, action) => {
+
+    },
+    editElement: (state, action) => {
+
+    }
+  }
+})
