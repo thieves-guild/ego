3c1d17e57ab71cb4fcfdf908c94d9c458c21d687
dmadridy <davidmadridpathway@gmail.com>
2023-06-05 18:07:52 -0500

    Simulating the addElement workflow

--- src/pages/elements.tsx
+++ src/pages/elements.tsx
@@ -1,8 +1,22 @@
-import React from 'react'
+import { useState } from "react"
 
 const Elements = () => {
+  const [elements, setElements] = useState([{
+    title: 'Health',
+    description: 'This includes eating well, sleeping 8 hours a day and doing excersise regularly'
+  }])
+
   return (
-    <div>Elements</div>
+    <div>
+      {elements.map((element) => {
+        return (
+          <div className="w-40 h-40 border border-black">
+            <h1>{element.title}</h1>
+            <p>{element.description}</p>
+          </div>
+        )
+      })}
+    </div>
   )
 }
 
