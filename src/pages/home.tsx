e7da93073ebe280262d228ce8b1978cf003c1fea
dmadridy <davidmadridpathway@gmail.com>
2023-06-08 07:28:10 -0500

    Solved stoic API issue

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -1,10 +1,12 @@
 import CircularBar from "../components/circular-progress-bar"
+import RandomQuote from "../components/random-quote"
 
 const Home = () => {
   return (
     <>
       <h1>My daily rate</h1>
       <CircularBar />
+      <RandomQuote />
     </>
   )
 }
