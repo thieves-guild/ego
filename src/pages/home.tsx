7efa798058823c9634c57c67632ee99504deccce
dmadridy <davidmadridpathway@gmail.com>
2023-06-10 16:41:36 -0500

    Stilying home page

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -3,11 +3,11 @@ import RandomQuote from "../components/random-quote"
 
 const Home = () => {
   return (
-    <>
+    <div className="flex flex-col items-center justify-center w-full p-20 gap-8">
       <h1>My daily rate</h1>
       <CircularBar />
       <RandomQuote />
-    </>
+    </div>
   )
 }
 
