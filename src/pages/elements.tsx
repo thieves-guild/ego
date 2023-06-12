572f335bafb2a4fddc67e776704b231b1d35b68b
dmadridy <davidmadridpathway@gmail.com>
2023-06-12 14:48:57 -0500

    add-element functinality working properly

--- src/pages/elements.tsx
+++ src/pages/elements.tsx
@@ -1,11 +1,13 @@
 import { useSelector } from "react-redux"
 import { RootState } from "../store"
+import AddElementPopUp from "../features/add-element/add-element-pop-up"
 
 const Elements = () => {
   const elements = useSelector((state: RootState) => state.element)
 
   return (
     <div>
+      <AddElementPopUp />
       {elements.map((element) => {
         return (
           <div className="w-40 h-40 border border-black">
