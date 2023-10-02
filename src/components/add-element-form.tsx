3ce320c485582562740914cc189aa135f35b8fb7
dmadridy <davidmadridpathway@gmail.com>
2023-10-02 06:51:47 -0500

    Installed radix-ui and started implementing it in add-element-form functionality

--- src/components/add-element-form.tsx
+++
@@ -1,5 +0,0 @@
-const AddElementForm = () => {
-  return <div>AddElementForm</div>;
-};
-
-export default AddElementForm;
--- src/pages/elements.tsx
+++ src/pages/elements.tsx
@@ -1,36 +1,10 @@
-import { useForm } from "react-hook-form";
+import AddElementForm from "../components/add-element-form";
 
 const Elements = () => {
-  const { register, handleSubmit } = useForm();
-  const onSubmit = () => null;
-
   return (
     <div>
       <h1>My Elements</h1>
-      <form
-        onSubmit={handleSubmit(onSubmit)}
-        className="flex flex-col border-2 w-fit p-4"
-        action=""
-      >
-        <label htmlFor="">Title</label>
-        <input
-          className="bg-neutral-100 p-2 rounded"
-          {...register("title")}
-          type="text"
-        />
-        <label htmlFor="">Description</label>
-        <input {...register("description")} type="text" />
-        <label htmlFor="">Percentage</label>
-        <input {...register("percentage")} type="number" />
-        <div className="flex gap-4 text-white justify-center">
-          <button className="bg-neutral-600 hover:bg-neutral-500 duration-100 rounded p-2 px-4">
-            Cancel
-          </button>
-          <button className="bg-green-600 hover:bg-green-500 duration-100 rounded p-2 px-4">
-            Add Element
-          </button>
-        </div>
-      </form>
+      <AddElementForm />
     </div>
   );
 };
